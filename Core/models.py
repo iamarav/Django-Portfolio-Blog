b4e41acf7975b0deb7f419a80cf0ccdd0da4f099
sachdeva.gaurav2911@gmail.com <sachdeva.gaurav2911@gmail.com>
2020-01-06 18:04:56 +0530

    Working on Contact Form

--- Core/models.py
+++ Core/models.py
@@ -12,3 +12,11 @@ class Feedback(models.Model):
 
     def __str__(self):
         return str(self.id)+": "+self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20,null=True, blank=True)
+    website = models.URLField(blank=True, null=True)
+    subject = models.CharField(max_length=500, null=True, blank=True)
+    message = models.TextField()
