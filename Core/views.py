161aa3d3f65ea0cb017b0e205e73a447b25ca142
sachdeva.gaurav2911@gmail.com <sachdeva.gaurav2911@gmail.com>
2020-01-06 11:43:42 +0530

    Working on About and Contact Pages

--- Core/views.py
+++ Core/views.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+
+# Define Global variables here
+media_url = settings.MEDIA_URL
+static = settings.STATIC_URL
 
 def HomePage(request):
     return render( request, 'template-home.html')
     # just to show some text on browser
 
 def About(request):
-    return render( request, 'template-about.html')  
+    return render( request, 'template-about.html', {'static_url': static})  
     # this renders the html page from template folder to frontent
 
 def ContactPage(request):
-    return render( request, 'template-contact.html', {'variable': 'value'})
+    return render( request, 'template-contact.html', {'static_url': static})
     # this renders the html page from template folder to frontent
 
 def LoginPage(request):
